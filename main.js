2ca19ed3d5fdd781dc2b4ec05430f7daccba166d
guiomie <guiomie@localhost.(none)>
2016-05-03 21:00:01 -0700

    fix missing module error

---
+++ main.js
@@ -0,0 +1,5 @@
+var FullnameAutoComplete = require('./fullname-autocomplete.js').FullnameAutoComplete;
+var Autocomplete = require('./autocomplete.js').AutoComplete;
+
+exports.FullnameAutoComplete = FullnameAutoComplete;
+exports.AutoComplete = AutoComplete;
